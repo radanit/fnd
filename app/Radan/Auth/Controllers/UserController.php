463dba858b7db127e503d8fc319fe6374f68d5ea
mehdiriahimanesh <m.riahimanesh@radanit.ir>
2019-02-01 12:18:14 +0330

    @BE: move user controller to auth module

--- app/Radan/Auth/Controllers/UserController.php
+++ app/Radan/Auth/Controllers/UserController.php
@@ -2,8 +2,14 @@
 
 namespace App\Radan\Auth\Controllers;
 
+use Validator;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use App\Http\Controllers\Controller;
+use App\Radan\Auth\Models\User;
+use App\Radan\Auth\Request\UserRequest;
+use App\Radan\Resources\UserResource;
+
 
 class UserController extends Controller
 {
@@ -14,7 +20,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        // Return 
+        return UserResource::collection(User::paginate());
     }
 
     /**
@@ -25,7 +32,31 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validation
+        $request->validate([
+            'username' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+            'active' => 'boolean',
+            'profile_id' => 'required|exists:profiles:id',
+            'profile_data' => 'required|json'
+        ]);
+
+        // First create user in users table     
+        $user = User::create([
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'active' => $request->active,
+        ]);
+
+        // Then create profile info base on profile type 
+        $user->userProfile()->create([           
+            'profile_id' => $request->profile_id,
+            'data' => $request->profile_date,
+        ]);
+
+        return new UserResource($user);
     }
 
     /**
@@ -36,7 +67,7 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
+        return new UserResource(User::findOrFail($id));        
     }
 
     /**
@@ -46,9 +77,29 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
-        //
+    public function update(UserRequest $request, $id)
+    {        
+        // Validation
+        $request->validate([           
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'string|min:6|confirmed',
+            'active' => 'boolean',
+            'profile_id' => 'exists:profiles:id',
+            'profile_data' => 'json'
+        ]);
+
+        // Update user profile data
+        $user = User::findOrFail($id);
+        $userProfile = $user->userProfile()->first();
+        $userProfile->profile_id = ($request->has('profile_id')) ? $request->profile_id: $userProfile->profile_id;
+        $userProfile->data = ($request->has('profile_data')) ? $request->profile_data: $userProfile->data;
+
+        $user->update($request->only(['email', 'password', 'active']));
+
+        
+
+
+        return new ProfileResource($profile);
     }
 
     /**
@@ -59,6 +110,13 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        // Find user by id
+        $user = User::findOrFail($id);
+
+        // Find user_profile record by user_id relation
+        $user->userProfile()->delete();
+        $user->delete();        
+        
+        return response()->json(null, 204);
     }
 }
