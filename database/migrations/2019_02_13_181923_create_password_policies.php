953d867d2a0424cebcd1e6a5bc0dc2fc1e9aa368
Mehdi Riahimanesh <m.riahimanesh@radanit.ir>
2019-02-14 12:31:07 +0330

    @BE: add column to password_policies

--- database/migrations/2019_02_13_181923_create_password_policies.php
+++ database/migrations/2019_02_13_181923_create_password_policies.php
@@ -16,9 +16,12 @@ class CreatePasswordPolicies extends Migration
         Schema::create('password_policies', function (Blueprint $table) {
             $table->increments('id');
             $table->int('min_length')->default(6);
-            $table->int('max_length')->nullable();
+            $table->int('max_length')->nullable()->default(NULL);
             $table->int('upper_case')->default(0);
             $table->int('lower_case')->default(0);
+            $table->int('digits')->default(6);
+            $table->int('special_chars')->default(0);
+            $table->string('does_not_contain')->nullable()->default(NULL);
             $table->timestamps();
         });
     }
