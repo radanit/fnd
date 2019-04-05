27a27e11c09d4818d2a006c677e21b6e85b56cc7
Mehdi Riahimanesh <m.riahimanesh@radanit.ir>
2019-04-06 02:02:56 +0430

    #BE-Bahar: change bahar table seeders file

---
+++ database/seeds/BaharProfileTableSeeder.php
@@ -0,0 +1,120 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Radan\Profile\Models\Profile;
+use App\Radan\Auth\Models\Role;
+
+class DoctorProfileTableSeeder extends Seeder 
+{
+
+	/**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+	public function run()
+	{		
+		//
+		Profile::create(array(
+			'name' => 'doctor',
+			'description' => 'پزشک',
+			'structure' => [
+				[
+					"id" => "1",
+					"name" => "first_name",
+					"item" => "el-input",
+					"type" => "string",
+					"required" => "true",
+					"label" => "profile.first_name",
+					"errorMsg" => "profile.first_nameError",
+					"rules" => "required|string|max:191",
+					
+				],
+				[
+					"id" => "2",
+					"name" => "last_name",
+					"item" => "el-input",
+					"type" => "string",
+					"required" => "true",
+					"label" => "profile.last_name",
+					"errorMsg" => "profile.last_nameError",
+					"rules" => "required|string|max:191",
+				],
+				[
+					"id" => "3",
+					"name" => "avatar",
+					"item" => "el-upload",
+					"type" => "file",
+					"required" => "false",
+					"label" => "profile.avatar",
+					"errorMsg" => "profile.avatarError",
+					"rules" => "bail|nullable|image|mimes:jpeg,jpg,png,gif|max:2048",
+					
+				],
+				[
+					"id" => "4",
+					"name" => "speciality",
+					"item" => "el-list",
+					"type" => "list",
+					"required" => "true",
+					"label" => "profile.speciality",
+					"errorMsg" => "profile.specialityError",
+					"rules" => "required|string|exists:specialities",					
+				],
+			]												
+		));	
+
+		Profile::create(array(
+			'name' => 'patient',
+			'description' => 'بیمار',
+			'structure' => [
+				[
+					"id" => "1",
+					"name" => "first_name",
+					"item" => "el-input",
+					"type" => "string",
+					"required" => "true",
+					"label" => "profile.first_name",
+					"errorMsg" => "profile.first_nameError",
+					"rules" => "required|string|max:191",
+					
+				],
+				[
+					"id" => "2",
+					"name" => "last_name",
+					"item" => "el-input",
+					"type" => "string",
+					"required" => "true",
+					"label" => "profile.last_name",
+					"errorMsg" => "profile.last_nameError",
+					"rules" => "required|string|max:191",
+				],
+				[
+					"id" => "3",
+					"name" => "avatar",
+					"item" => "el-upload",
+					"type" => "file",
+					"required" => "false",
+					"label" => "profile.avatar",
+					"errorMsg" => "profile.avatarError",
+					"rules" => "bail|nullable|image|mimes:jpeg,jpg,png,gif|max:2048",
+					
+				],				
+			]												
+		));	
+		
+		Role::create([            
+                'name' => 'doctor',
+                'display_name' => 'پزشک', 
+                'description'=> 'پزشک',                
+            ]
+		);
+		
+		Role::create([            
+                'name' => 'patient',
+                'display_name' => 'بیمار', 
+                'description'=> 'بیمار',                
+            ]
+        );
+	}
+}
