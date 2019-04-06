b88f4d63eb432726edf68248db438c7e5c18c561
mehdiriahimanesh <m.riahimanesh@radanit.ir>
2019-04-06 23:44:09 +0430

    #BE: add index and show function to Master class APIController

--- app/Radan/Http/Controllers/APIController.php
+++ app/Radan/Http/Controllers/APIController.php
@@ -39,8 +39,8 @@ class APIController extends BaseController
             return $next($request);
         });       
     } 
-	
-	/**
+
+    /**
      * Return number of records return per page
      *
      * @return integer
@@ -49,4 +49,57 @@ class APIController extends BaseController
 	{
 		return $this->config->get('radan.pagination.count',15);                    
     }
+        
+    /**
+     * Fetch all records of model
+     *
+     * @return Model
+     */
+    protected function all($model)
+    {
+        // Determinde number of record to return
+        $pgCount = $this->getPaginationCount();
+        $records = ($pgCount) ? $model->paginate($pgCount) : $model->get();
+        return $records;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {        
+        // Get all resources
+        $model = new $this->model();
+        $model = isset($this->relations) ? $model->with($this->relations):$model;
+
+        // Return api resource
+        if (isset($this->jsonResource)) {
+            return $this->jsonResource::collection($this->all($model));
+        } else {
+            return $this->all($model);
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Integer    $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $model = new $this->model();
+        if (isset($this->relations)) {
+            $record = $model->with($this->relations)->findOrFail($id);
+        }
+        else {
+            $record = $model->findOrFail($id);
+        }
+                
+        // Return JSON response
+        return new $this->jsonResource($record);
+    }    
 }
