064fc36a458a874ed8433e3ab4fdeb1913e27671
Ehsan bagherzadegan <bagherzadegan.e@gmail.com>
2019-05-03 21:26:11 +0430

    @FE:Editor

--- resources/assets/js/components/reception/captured/ReceptionOpnion.vue
+++ resources/assets/js/components/reception/captured/ReceptionOpnion.vue
@@ -1,13 +1,31 @@
 <template>
-    <div class="continer">
-        editor
-    </div>
-</template>
-<style>
-
-</style>
-<script>
-export default {
-    
-}
-</script>
+   <div id="container">
+     <vue-editor v-model="content" :editorToolbar="customToolbar"></vue-editor>
+   </div>
+ </template>
+ 
+ <script>
+   import { VueEditor } from 'vue2-editor'
+ 
+   export default {
+ 
+   components: {
+      VueEditor
+   },
+ 
+   data() {
+       return {
+         content: '<h1>نظر پزشک</h1>',
+         customToolbar: [
+            [{ 'header': [false, 1, 2, 3, 4, 5, 6, ] }],
+            [{ 'size': ['small', false, 'large', 'huge'] }],
+            ['bold', 'italic', 'underline','strike'],
+            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+            [{ 'align': 'center'}, { 'align': 'justify' },{ 'align': 'right'},{ 'align': '' }],
+            [{ 'color': [] }, { 'background': [] }],
+            [{ 'direction': 'rtl' }],
+          ] 
+       }
+     }
+   }
+ </script> 
