e9969e09e28ec3c5338d5f8f994a09676e0ec4d1
Mehdi Riahimanesh <m.riahimanesh@radanit.ir>
2019-06-21 16:39:13 +0430

    #BE: fix download capture api access

--- routes/api.php
+++ routes/api.php
@@ -59,10 +59,16 @@ Route::group(['middleware' => 'auth:api'], function() {
  */
 Route::group(['middleware' => ['auth:api','ability:admin,can-capture-reception']], function() {
     Route::get('receptions/capture', 'API\ReceptionCaptureController@index');       
-    Route::get('receptions/{reception}/capture', 'API\ReceptionCaptureController@show');
-    Route::get('receptions/{reception}/capture/{capture}', 'API\ReceptionCaptureController@download');   
+    Route::get('receptions/{reception}/capture', 'API\ReceptionCaptureController@show');    
     Route::put('receptions/{reception}/capture', 'API\ReceptionCaptureController@update');    
     Route::delete('receptions/{reception}/capture', 'API\ReceptionCaptureController@destroy');    
+});
+
+/**
+ * Reception Download RadioGraphys APIs
+ */
+Route::group(['middleware' => ['auth:api']], function() {
+    Route::get('receptions/{reception}/capture/{capture}', 'API\ReceptionCaptureController@download');   
 });
 
 /**
