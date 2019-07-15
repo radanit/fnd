da61a8e1b9fd9458ac3af62327a31e6780650470
mehdiriahimanesh <m.riahimanesh@radanit.ir>
2019-07-15 18:03:02 +0430

    #BE: add created_at to reception result resource

--- app/Http/Resources/ReceptionResultCollection.php
+++ app/Http/Resources/ReceptionResultCollection.php
@@ -17,6 +17,7 @@ class ReceptionResultCollection extends JsonResource
         return [ 
             'id' => $this->id,
             'result' => $this->result,
+            'created_at' => $this->created_at,
         ];
     }
 }
