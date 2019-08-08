45d6305719cfa4d56c676ed2e9d0ef31e6072c1c
mehdiriahimanesh <m.riahimanesh@radanit.ir>
2019-08-08 15:18:14 +0430

    repaire deskyo only css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -66,12 +66,12 @@
             </div>
         </div>
         <div class="col-md-8">		
-			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+			<div id="desktop-only" class="carousel slide" data-ride="carousel">
 				<ol class="carousel-indicators">
-					<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-					<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-					<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-					<li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+					<li data-target="#desktop-only" data-slide-to="0" class="active"></li>
+					<li data-target="#desktop-only" data-slide-to="1"></li>
+					<li data-target="#desktop-only" data-slide-to="2"></li>
+					<li data-target="#desktop-only" data-slide-to="4"></li>
 				</ol>
 				<div class="carousel-inner">
 					<div class="carousel-item active">
@@ -87,11 +87,11 @@
 					<img class="d-block w-100 h-100" src="{{URL::asset('/assets/images/slideshows/4.jpg')}}" style="vertical-align:center;" alt="First slide">
 					</div>
 				</div>
-				<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<a class="carousel-control-prev" href="#desktop-only" role="button" data-slide="prev">
 					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 					<span class="sr-only">Previous</span>
 				</a>
-				<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<a class="carousel-control-next" href="#desktop-only" role="button" data-slide="next">
 					<span class="carousel-control-next-icon" aria-hidden="true"></span>
 					<span class="sr-only">Next</span>
 				</a>
