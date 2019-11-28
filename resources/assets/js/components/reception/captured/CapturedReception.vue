02bb73812762e16c58ff1c9c79cffee7b4e5b9c1
Ehsan bagherzadegan <bagherzadegan.e@gmail.com>
2019-11-28 17:18:52 +0330

    @FE

--- resources/assets/js/components/reception/captured/CapturedReception.vue
+++ resources/assets/js/components/reception/captured/CapturedReception.vue
@@ -19,7 +19,7 @@
               <!-- /.card-header -->
               <div class="card-body table-responsive p-0">                
 				<el-table
-					:data="tableData.filter(data => !search || data.patient.fullname.toLowerCase().includes(search.toLowerCase())|| data.patient.national_id.toLowerCase().includes(search.toLowerCase())|| data.patient.mobile.toLowerCase().includes(search.toLowerCase()))"
+					:data="list.filter(data => !search || data.patient.fullname.toLowerCase().includes(search.toLowerCase())|| data.patient.national_id.toLowerCase().includes(search.toLowerCase())|| data.patient.mobile.toLowerCase().includes(search.toLowerCase()))"
           :default-sort = "{prop: 'id', order: 'descending'}"
 					style="width: 100%"
           :empty-text = "trans('app.no_data_found')"
