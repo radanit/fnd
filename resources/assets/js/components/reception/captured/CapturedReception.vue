b8500f6a38023f56219a0138c71844cc88cc21e3
Ehsan bagherzadegan <bagherzadegan.e@gmail.com>
2019-11-28 17:21:53 +0330

    @FE

--- resources/assets/js/components/reception/captured/CapturedReception.vue
+++ resources/assets/js/components/reception/captured/CapturedReception.vue
@@ -19,6 +19,7 @@
               <!-- /.card-header -->
               <div class="card-body table-responsive p-0">                
 				<el-table
+           height="624"
 					:data="list.filter(data => !search || data.patient.fullname.toLowerCase().includes(search.toLowerCase())|| data.patient.national_id.toLowerCase().includes(search.toLowerCase())|| data.patient.mobile.toLowerCase().includes(search.toLowerCase()))"
           :default-sort = "{prop: 'id', order: 'descending'}"
 					style="width: 100%"
